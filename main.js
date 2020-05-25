18d9e1d2d4c51bd5d309aff20b4f6d053769de6f
JayceLai <goodlaijiaxin@qq.com>
2020-05-25 17:06:59 +0800

    mesh optimiser plugin base cocos creator 3d v1.1

---
+++ main.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+module.exports = {
+
+    // 当 package 被正确加载的时候执行
+    load () { },
+
+    // 当 package 被正确卸载的时候执行
+    unload () { },
+
+    // 插件所监听的消息
+    messages: {
+        'say-hello' () {
+            console.log('Hello World!');
+        },
+        'open' () {
+            Editor.Panel.open('mesh-optimizer');
+        },
+    },
+};
